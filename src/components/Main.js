622e1052da62a35b6477e98a7f8893d33904ad98
Jefferson Alves <jeeffsantoos@outlook.com>
2023-06-08 00:18:55 -0300

    Aula React

---
+++ src/components/Main.js
@@ -0,0 +1,109 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React, { Component } from 'react';
+import Form from './Form';
+import Tarefas from './Tarefas';
+import './Main.css';
+
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      novaTarefa: '',
+      tarefas: [],
+      index: -1,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  componentDidMount() {
+    const tarefas = JSON.parse(localStorage.getItem('tarefas'));
+
+    if (!tarefas) return;
+
+    this.setState({
+      tarefas,
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { tarefas } = this.state;
+
+    if (tarefas === prevState.tarefas) return;
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }
+
+  handleChange(e) {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  }
+
+  handleEdit(e, index) {
+    const { tarefas } = this.state;
+
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
+  }
+
+  handleDelete(e, index) {
+    const { tarefas } = this.state;
+    const novasTarefas = [...tarefas];
+    novasTarefas.splice(index, 1);
+
+    this.setState({
+      tarefas: [...novasTarefas],
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  handleSubmit(e) {
+    e.preventDefault();
+    const { tarefas, index } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    if (!novaTarefa) return;
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+    const novasTarefas = [...tarefas];
+
+    if (index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: '',
+      });
+    } else {
+      novasTarefas[index] = novaTarefa;
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+      });
+    }
+  }
+
+  render() {
+    const { novaTarefa, tarefas } = this.state;
+    return (
+      <div className="main">
+        <h1>Lista de tarefas</h1>
+        <Form
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          novaTarefa={novaTarefa}
+        />
+        <Tarefas
+          tarefas={tarefas}
+          handleEdit={this.handleEdit}
+          handleDelete={this.handleDelete}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/Tarefas/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
+import './Tarefas.css';
+
+export default function Tarefas({ tarefas, handleEdit, handleDelete }) {
+  return (
+    <ul className="tarefas">
+      {tarefas.map((tarefa, index) => (
+        <li key={tarefa}>
+          {tarefa}
+          <div>
+            <FaEdit onClick={(e) => handleEdit(e, index)} className="edit" />
+            <FaWindowClose onClick={(e) => handleDelete(e, index)} className="delete" />
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+Tarefas.propTypes = {
+  handleEdit: PropTypes.func.isRequired,
+  handleDelete: PropTypes.func.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  tarefas: PropTypes.array.isRequired,
+};
